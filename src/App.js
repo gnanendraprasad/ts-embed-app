57a91d5517daaa0e661f353baf38e7babb78e3d8
Gnanendra Prasad T <32493586+gnanendraprasad@users.noreply.github.com>
2025-06-16 12:08:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
     {
       key: 'dashboard1',
       label: 'Sales Dashboard',
-      liveboardId: 'abc123sales',
-      vizId: 'sales-viz-1',
+      liveboardId: 'ca34dc1c-8a8d-4cb7-bb29-5c50d3d67bd1',
+      vizId: 'a1cdfa62-6db6-446b-9cc2-692a9d26ee2f',
     },
     {
       key: 'dashboard2',
