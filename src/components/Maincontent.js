b26d7302b68042b75974db631a696a78e3da8b5a
Gnanendra Prasad T <32493586+gnanendraprasad@users.noreply.github.com>
2025-06-18 16:50:51 -0400

    Updated file structure

--- src/components/Maincontent.js
+++ src/components/Maincontent.js
@@ -1,28 +1,40 @@
-import React, {useState} from "react";
-import Stellantis from "./SidebarItems/Stellantis"
+import React, { useState } from "react";
+import Stellantis from "./SidebarItems/Stellantis";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
-const Maincontent = ({selectedMenu, menuItems}) => {
-    
-      const [policyId, setPolicyId] = useState("");
-      const [deviceId, setDeviceId] = useState("");
-      const [tripId, setTripId] = useState("");
-    
-    const selectedItem = menuItems.find((item) => item.key === selectedMenu);
+const Maincontent = ({ selectedMenu, menuItems }) => {
+  const [policyId, setPolicyId] = useState("");
+  const [deviceId, setDeviceId] = useState("");
+  const [tripId, setTripId] = useState("");
+
+  const selectedItem = menuItems.find((item) => item.key === selectedMenu);
 
   return (
     <>
-    {selectedMenu === "Stellantis" && (
-          <Stellantis            
-            selectedItem={selectedItem}
-            policyId={policyId}
-            setPolicyId={setPolicyId}
-            deviceId={deviceId}
-            setDeviceId={setDeviceId}
-            tripId={tripId}
-            setTripId={setTripId}
-          />
-        )    
-    }
+      <Tabs
+        defaultActiveKey="profile"
+        id="fill-tab-example"
+        className="mb-3"
+        fill
+      >
+        <Tab eventKey="gps" title="GPS">
+          {selectedMenu === "Stellantis" && (
+            <Stellantis
+              selectedItem={selectedItem}
+              policyId={policyId}
+              setPolicyId={setPolicyId}
+              deviceId={deviceId}
+              setDeviceId={setDeviceId}
+              tripId={tripId}
+              setTripId={setTripId}
+            />
+          )}
+        </Tab>
+        <Tab eventKey="opt2" title="Option2">
+
+        </Tab>
+      </Tabs>
     </>
   );
 };
